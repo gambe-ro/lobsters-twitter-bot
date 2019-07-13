672d2e99ef6bf384596a1dee983c3d8859da399e
Giacomo Mariani <karjudev@gmail.com>
2019-07-13 10:59:10 +0200

    Tweets every story posted in the interval

--- src/bot.py
+++ src/bot.py
@@ -1,9 +1,9 @@
 from os import getenv
 from time import sleep
+from datetime import datetime
 
 from tweepy import OAuthHandler, API
 from requests import get
-
 from parse import parse
 
 import schedule
@@ -25,65 +25,11 @@ TWEET_FORMAT = "{title} - {short_id_url} ({author}, {created_at}) {hashtags}"
 auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 
-def get_newest_story () -> dict:
+def get_last_posted_tweet (bot: API) -> dict:
     """
-    Gets the newest story posted on the site.
+    Gets the last tweet posted and parses it.
 
-    :return: Dictionary with data of the newest story in the JSON.
-    """
-    # Gets the webpage
-    request = get(JSON_URL)
-    # Extrapolates JSON content
-    json_result = request.json()
-    # Gets first element
-    story = json_result[0]
-    # Modifies internal structure to facilitate parsing
-    story["author"] = story["submitter_user"]["username"]
-    # Returns the story
-    return story
-
-def is_newest (story_a: dict, story_b: dict) -> bool:
-    """
-    Returns True if the first story is newest than the second one.
-
-    :param story_a: Story to be verified as newest.
-    :param story_b: Other story to be compared.
-    :return: True if story_a is newest than story_b, False otherwise.
-    """
-    # If there is no second story, first is newest.
-    if (story_b is None):
-        return True
-    # If created_at fields are different, first story is the newest.
-    # TODO: Find more efficient and elegant way of compare stories
-    return (story_a["created_at"] > story_b["created_at"])
-
-def build_tweet_string (story: dict) -> str:
-    """
-    Builds a string to be tweeted.
-
-    :param story: Story to build tweet on.
-    :return: String to be tweeted
-    """
-    # Transforms story's tags in hashtags
-    hashtag_list = list(map(lambda tag: f"#{tag}", story["tags"]))
-    # Joins hashtags as list
-    hashtags = " ".join(hashtag_list)
-    # Builds the base string
-    base_string = TWEET_FORMAT.format(
-        title=story["title"],
-        author=story["author"],
-        created_at=story["created_at"],
-        short_id_url=story["short_id_url"],
-        hashtags=hashtags
-    )
-    # Returns string
-    return base_string
-
-def get_latest_posted_tweet (bot: API) -> dict:
-    """
-    Gets the latest tweet posted and parses it.
-
-    :param bot: Twitter bot to fetch the tweet from
+    :param bot: Twitter bot to fetch the tweet from.  
     :return: Dictionary with "title", "short_id_url", "author" and "created_at" fields
     """
     # Gets its own Twitter ID
@@ -94,24 +40,81 @@ def get_latest_posted_tweet (bot: API) -> dict:
     result = parse(TWEET_FORMAT, latest_tweet.full_text)
     # Returns dictionary with the required fields, or None if the parsing has failed
     return result.named if result else None
+
+def is_newest (story_a: dict, story_b: dict) -> bool:
+    """
+    Returns True if the first story is newest than the second one.
+
+    :param story_a: Story to be verified as newest.  
+    :param story_b: Other story to be compared.  
+    :return: True if story_a is newest than story_b, False otherwise.
+    """
+    # If created_at fields are different, first story is the newest.
+    # TODO: Find more efficient and elegant way of compare stories
+    return (story_a["created_at"] > story_b["created_at"])
+
+def get_new_stories (latest_story: dict) -> [dict]:
+    """
+    Gets the stories posted on the site after story.
+
+    :param latest_story: Latest story published as a tweet.  
+    :return: List of stories published after story, or the latest published intem on site if story doesn't exists.
+    """
+    # Gets the webpage
+    request = get(JSON_URL)
+    # Extrapolates JSON content
+    json_result = request.json()
+    # List of stories published after story
+    stories = []
+    # If latest story doesn't exists (this is the first tweet) gets only the latest story
+    if (latest_story is None): stories.append(json_result[0])
+    # Else gets the latest stories published
+    else:
+        for story in json_result:
+            if (not is_newest(story, latest_story)): break
+            stories.append(story)
+    # Reverts list (from older to newer) and returns it
+    stories.reverse()
+    return stories
+
+def build_tweet_string (story: dict) -> str:
+    """
+    Builds a string to be tweeted.
+
+    :param story: Story to build tweet on.  
+    :return: String to be tweeted
+    """
+    # Transforms story's tags in hashtags
+    hashtag_list = list(map(lambda tag: f"#{tag}", story["tags"]))
+    # Joins hashtags as list
+    hashtags = " ".join(hashtag_list)
+    # Builds the base string
+    base_string = TWEET_FORMAT.format(
+        title=story["title"],
+        author=story["submitter_user"]["username"],
+        created_at=story["created_at"],
+        short_id_url=story["short_id_url"],
+        hashtags=hashtags
+    )
+    # Returns string
+    return base_string
     
 def main ():
     # Creates bot
     bot = API(auth)
     print("Checking for new posts...")
-    # Fetches JSON for newest story
-    fetched_story = get_newest_story()
-    # Fetches Twitter for the last published story
-    last_posted_tweet = get_latest_posted_tweet(bot)
-    # If the latest story is newest than the last posted, tweets it
-    if (is_newest(fetched_story, last_posted_tweet)):
-        # Builds tweet string
-        tweet = build_tweet_string(fetched_story)
-        # Posts tweet
-        bot.update_status(tweet)
-        print("Posted tweet: {tweet}".format(tweet=tweet))
-    # Else prints a sleepy message
-    else: print("Nothing new here, the bot goes back to sleep.")
+    # Fetches Twitter for the last published stories
+    last_posted_tweet = get_last_posted_tweet(bot)
+    # Fetches JSON for new stories
+    new_stories = get_new_stories(last_posted_tweet)
+    # Tweets all the new stories
+    print("[{time}]".format(time=datetime.now()), end=" ")
+    if (len(new_stories) == 0): print("Nothing new here, the bot is back to sleep.")
+    else:
+        for story in new_stories:
+            tweet = build_tweet_string(story)
+            bot.update_status(tweet)
+            print("Tweeted: {tweet}".format(tweet=tweet))
 
 if __name__ == "__main__":
     schedule.every(FETCH_INTERVAL).minutes.do(main)
